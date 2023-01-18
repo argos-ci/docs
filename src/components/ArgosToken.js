22e27e873fe08607aea3e6500a1653675e66c0d6
Jeremy Sfez <jeremy@smooth-code.com>
2023-01-18 16:32:40 +0100

    feat: update ci config

--- src/components/ArgosToken.js
+++ src/components/ArgosToken.js
@@ -1,47 +1,39 @@
 import * as React from "react";
 import Code from "@theme/MDXComponents/Code";
-import gearButton from "@site/static/img/gear-button.png";
+import BlocCode from "@theme/CodeBlock";
 
-export const GetArgosToken = () => (
-  <p>
-    To get token value, <a href="https://app.argos-ci.com">sign in to Argos</a>{" "}
-    and click on the{" "}
-    <img
-      src={gearButton}
-      alt="Gear button"
-      className="rounded"
-      width={40}
-      style={{ display: "inline-block", marginBottom: -8 }}
-    />
-    button in front of your repository.
-  </p>
-);
-
-export const SetArgosTokenSecret = () => (
-  <p>
-    Set the <Code>ARGOS_TOKEN</Code> as an environment variable (sometimes
-    called “secrets”) in your CI configuration.
-  </p>
-);
-
-export const SetArgosToken = () => (
-  <p>
-    Set the <Code>ARGOS_TOKEN</Code> as an environment variable in your CI
-    configuration.
-  </p>
+export const InstallCliCommand = () => (
+  <>
+    <p>
+      Install the{" "}
+      <a href="https://www.npmjs.com/package/@argos-ci/cli">@argos-cli</a>{" "}
+      package from npm:
+    </p>{" "}
+    <BlocCode>npm i --save-dev @argos-cli</BlocCode>
+  </>
 );
 
 export const WhyArgosToken = () => (
   <p>
-    Argos uses the <Code>ARGOS_TOKEN</Code> to identify to which repository the
-    screenshots you send are related.
+    Argos uses a token called <code>ARGOS_TOKEN</code> to identify to which
+    repository the screenshots you send are related to.
   </p>
 );
 
-export const TokenPrivacyWarningMessage = () => (
-  <blockquote>
-    <b>Warning:</b> Be careful to store your Argos token safely.{" "}
-    <Code>ARGOS_TOKEN</Code> environment variable should be accessible by your
-    CI but not visible publicly.
-  </blockquote>
+export const RepositoryExampleLink = () => (
+  <p>
+    For a repository examples, check out our{" "}
+    <a href="https://github.com/argos-ci/argos/tree/main/examples">
+      GitHub repository
+    </a>
+    .
+  </p>
+);
+
+export const Enjoy = () => (
+  <p>
+    Congratulations, now that Argos is installed, your app screenshots will be
+    uploaded to Argos by your CI. You are ready to start the{" "}
+    <a href="/visual-testing">visual testing</a> routine!
+  </p>
 );
