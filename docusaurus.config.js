568989879ed9037c4a7a94ed68de13c7ab24bc20
Greg Bergé <berge.greg@gmail.com>
2024-05-28 10:16:06 +0200

    feat: document monitoring mode (#131)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,10 @@ const config = {
       }),
     ],
   ],
+  themes: ["@docusaurus/theme-mermaid"],
+  markdown: {
+    mermaid: true,
+  },
   plugins: [
     function myPlugin(context, options) {
       return {
